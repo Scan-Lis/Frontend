c7845b0da77ff37c0868a0347bccb0fbae89d9c5
Jose Waldo <josferw@gmail.com>
2024-03-06 09:47:36 -0500

    Style: añadi configuración de colores y pesos de fuentes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "light-blue": "#75C1C0",
+        "dark-blue": "#0E7774",
+        "ultra-dark-blue": "#00413F",
+        "light-gray": "#686868",
+        "dark-gray": "#2C2C2C",
+      },
+      fontWeight: {
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
