ae8ddb23607da9a80b9ff06e1be7c037ba3bf12b
Jose Waldo <josferw@gmail.com>
2024-03-13 11:18:53 -0500

    Style: se añadió un degradado

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -25,9 +26,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-linear":
+          "linear-gradient(153deg, rgba(117,193,192,1) 0%, rgba(14,119,116,1) 100%)",
       },
     },
   },
-  plugins: [],
 };
+
 export default config;
