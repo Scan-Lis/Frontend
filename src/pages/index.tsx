a15a3eca2f0e098e2488d6e4f206d04b15ed1cf5
Jose Waldo <josferw@gmail.com>
2024-03-13 11:23:50 -0500

    Feat: se limpió la página de aterrizaje

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,3 @@
-import { Urbanist } from "next/font/google";
-
-const urbanist = Urbanist({
-  subsets: ["latin"],
-  weight: ["500", "600", "700", "800"],
-});
-
 export default function Home() {
-  return (
-    <main className={`${urbanist.className}`}>
-      <h1>Scan-Lis</h1>
-    </main>
-  );
+  return <h1>Scan-Lis</h1>;
 }
