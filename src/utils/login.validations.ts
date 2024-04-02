9d9112d41deed53b736377976ad654b4c3d03984
Jose Waldo <josferw@gmail.com>
2024-04-02 11:24:22 -0500

    Fix: se arregló el problema del build

--- src/utils/login.validations.ts
+++ src/utils/login.validations.ts
@@ -1,4 +1,4 @@
-import { LoginFields } from "@/types";
+import { LoginFields } from "@/types/types";
 
 const isEmailValided = (email: string) => {
   const emailRegex = /^[a-zA-Z0-9._-]+@udea\.edu\.co$/;
