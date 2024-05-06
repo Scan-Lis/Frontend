c75f36a55abad108ea32e90486f45258cf55dc5b
Jose Waldo <josferw@gmail.com>
2024-05-06 13:46:58 -0500

    Feat: se modificó el componente para que fuera reutilizable

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -2,15 +2,30 @@ import { cn } from "@/utils/classnames";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
 
+type Option = {
+  value: string;
+  label: string;
+};
+
 interface DropdownProps {
-  options: string[];
+  options: Option[];
+  setSelectedOption: (option: string) => void;
+  selectedOption?: string;
+  labelDropdown: string;
 }
 
-const Dropdown = ({ options }: DropdownProps) => {
+const Dropdown = ({
+  labelDropdown,
+  options,
+  selectedOption,
+  setSelectedOption,
+}: DropdownProps) => {
   const [isOpen, setisOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedLabel, setSelectedLabel] = useState("");
 
-  const handleOpen = () => {
+  const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
     setisOpen(!isOpen);
   };
 
@@ -21,28 +36,33 @@ const Dropdown = ({ options }: DropdownProps) => {
 
   const classes = {
     options: cn(
-      "absolute z-20 bg-light-blue text-white p-4 w-full rounded-md flex flex-col gap-2 mt-2 opacity-0 transition-all",
-      { "opacity-100": isOpen }
+      "absolute z-20 bg-light-blue text-white p-4 w-full rounded-md flex flex-col gap-2 mt-2 hidden opacity-0 transition-all",
+      { "block opacity-100": isOpen }
     ),
-    item: cn("hover:bg-white/35 cursor-pointer py-1 px-2 rounded-md"),
+    item: cn("hover:bg-white/35 cursor-pointer py-1 px-2 rounded-md "),
     icon: cn("w-6 h-6 rotate-0 transition-all", { "rotate-180": isOpen }),
   };
 
   return (
-    <div className="relative min-w-[8rem]">
+    <div className="relative min-w-[12rem]">
       <button
-        onClick={handleOpen}
+        onClick={(e) => handleOpen(e)}
         className="flex gap-2 items-center justify-center w-full bg-light-blue/35 rounded-md py-2 px-4 font-semibold">
-        {selectedOption || "Seleccionar sala"}
+        {selectedLabel || labelDropdown}
         <ChevronDownIcon className={classes.icon} />
       </button>
       <ul className={classes.options}>
-        {options.map((option, index) => (
-          <li
-            key={index}
-            className={classes.item}
-            onClick={() => handleSelect(option)}>
-            {option}
+        {options.map(({ label, value }, index) => (
+          <li key={index} className={classes.item}>
+            <button
+              className="w-full"
+              onClick={(e) => {
+                e.preventDefault();
+                handleSelect(value);
+                setSelectedLabel(label);
+              }}>
+              {label}
+            </button>
           </li>
         ))}
       </ul>
