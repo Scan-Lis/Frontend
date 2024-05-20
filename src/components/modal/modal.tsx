20721b376dba790640f32b41e67133672b48b4c2
Jose Waldo <josferw@gmail.com>
2024-05-20 18:15:17 -0500

    Feat: se añadió el componente de modal

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,57 @@
+import { cn } from "@/utils/classnames";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { ReactNode, useEffect } from "react";
+
+export interface ModalProps {
+  title?: ReactNode;
+  children: ReactNode;
+  closeModal: () => void;
+}
+
+const Modal = ({ children, closeModal, title }: ModalProps) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const classes = {
+    modal: cn(
+      "relative bg-white p-4 rounded-2xl fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5 max-w-xl"
+    ),
+    close: cn(
+      "absolute right-4 bg-light-blue/30 p-1 rounded-md hover:bg-light-blue/40"
+    ),
+    bgShadow: cn("w-full h-full fixed top-0 left-0 z-10 bg-black/30"),
+    childrenWrapper: cn("mt-4"),
+    headerWrapper: cn("flex justify-between"),
+  };
+
+  return (
+    <>
+      <div className={classes.bgShadow}></div>
+      <article className={classes.modal}>
+        <div className={classes.headerWrapper}>
+          {title && (
+            <header className="mt-4 text-center w-full font-bold uppercase text-lg border-b-[1px] text-dark-blue border-b-gray-300/60 pb-3">
+              {title}
+            </header>
+          )}
+          <button className={classes.close} onClick={closeModal}>
+            <XMarkIcon className="w-5 h-5" />
+          </button>
+        </div>
+
+        <div className={classes.childrenWrapper}>{children}</div>
+      </article>
+    </>
+  );
+};
+
+export default Modal;
---
+++ src/components/modal/useModal.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+interface ModalHookReturn {
+  openModal: () => void;
+  closeModal: () => void;
+  isOpen: boolean;
+}
+
+const useModal = (): ModalHookReturn => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return {
+    openModal,
+    closeModal,
+    isOpen,
+  };
+};
+
+export default useModal;
