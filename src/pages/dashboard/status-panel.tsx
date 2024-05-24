ced25bbb8a7a969bf0d0a741fa8ed76844ff0030
Jose Waldo <josferw@gmail.com>
2024-05-24 14:45:28 -0500

    Feat: se añadió el custom hook de seo

--- src/pages/dashboard/status-panel.tsx
+++ src/pages/dashboard/status-panel.tsx
@@ -1,7 +1,12 @@
 import Layout from "@/components/layout";
+import useSEO from "@/hooks/useSEO";
 import StatusPanelModule from "@/modules/computers/list";
 
 const StatusPanelPage = () => {
+  useSEO({
+    title: "Panel de Estado",
+    description: "Panel de estado de los equipos",
+  });
   return (
     <Layout>
       <StatusPanelModule />
