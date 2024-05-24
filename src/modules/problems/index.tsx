0271ed4a6b3c47819e70393305b5011d30f1a978
Jose Waldo <josferw@gmail.com>
2024-05-24 15:17:27 -0500

    Feat: se añadió la página para los problemas

---
+++ src/modules/problems/index.tsx
@@ -0,0 +1,11 @@
+const ProblemsModule = () => {
+  return (
+    <main className="">
+      <h1 className="font-bold text-4xl text-dark-blue">
+        Página en construcción
+      </h1>
+    </main>
+  );
+};
+
+export default ProblemsModule;
---
+++ src/pages/dashboard/problems.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout";
+import ProblemsModule from "@/modules/problems";
+
+const ProblemsPage = () => {
+  return (
+    <Layout>
+      <ProblemsModule />
+    </Layout>
+  );
+};
+
+export default ProblemsPage;
