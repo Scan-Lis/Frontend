69ce75575f18f60075b4d3a3c8d3ed193aa51f22
Jose Waldo <josferw@gmail.com>
2024-05-24 14:45:54 -0500

    Feat: se añadió el custom hook de seo

--- src/pages/form-report/[[...slug]].tsx
+++ src/pages/form-report/[[...slug]].tsx
@@ -1,3 +1,4 @@
+import useSEO from "@/hooks/useSEO";
 import FormReportModule from "@/modules/form-report";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -5,6 +6,11 @@ import { useEffect } from "react";
 const FormReport = () => {
   const router = useRouter();
   const { slug } = router.query;
+
+  useSEO({
+    title: "Formulario de Reporte",
+    description: "Reportar un problema con un equipo",
+  });
 
   useEffect(() => {
     if (slug) {
