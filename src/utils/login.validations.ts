ca86a45c7b8a5e44cabb1707a3d8b23cdd2a421f
Jose Waldo <josferw@gmail.com>
2024-05-24 15:00:08 -0500

    Se removió un archivo

--- src/utils/login.validations.ts
+++
@@ -1,6 +0,0 @@
-import { LoginFields } from "@/types/types";
-
-const isEmailValided = (email: string) => {
-  const emailRegex = /^[a-zA-Z0-9._-]+@udea\.edu\.co$/;
-  return emailRegex.test(email);
-};
