b82c7709d9815030e68c3f7b970fed19341882fc
Jose Waldo <josferw@gmail.com>
2025-06-09 07:56:48 -0500

    feat(http.ts): add request interceptor to include JWT in headers for authenticated requests to improve security and session management

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
 // TODO: Cambiar esta implementacion usando el patron de Singleton
 
@@ -7,4 +8,12 @@ export const http = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+http.interceptors.request.use(async (config) => {
+  const session = await getSession();
+  if (session?.jwt) {
+    config.headers.Authorization = `Bearer ${session.jwt}`;
+  }
+  return config;
 });
